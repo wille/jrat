2283dbde23f35c9a7d4770020964624a86324089
redpois0n <red@cmail.nu>
2013-04-27 10:40:16 +0200

    PluginClassLoader update, 3.2.3.7 upcoming

--- src/org/jrat/project/api/PluginClassLoader.java
+++ src/org/jrat/project/api/PluginClassLoader.java
@@ -1,8 +1,21 @@
 package org.jrat.project.api;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class PluginClassLoader extends ClassLoader {
 
 	public PluginClassLoader(ClassLoader parent) {
 		super(parent);
+	}
+	
+	@Override
+	public InputStream getResourceAsStream(String resource) {
+		return null;
+	}
+	
+	@Override
+	public URL getResource(String resource) {
+		return null;
 	}
 }
