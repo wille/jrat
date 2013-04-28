e05feea9281d73680527628f3dccb86c26d6e36f
redpois0n <red@cmail.nu>
2013-04-28 09:41:55 +0200

    PluginClassLoader resource loading

--- src/org/jrat/project/api/PluginClassLoader.java
+++ src/org/jrat/project/api/PluginClassLoader.java
@@ -1,6 +1,11 @@
 package org.jrat.project.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class PluginClassLoader extends ClassLoader {
@@ -17,5 +22,28 @@ public class PluginClassLoader extends ClassLoader {
 	@Override
 	public URL getResource(String resource) {
 		return null;
+	}
+	
+	/**
+	 * 
+	 * @param folder
+	 * @param resource
+	 * @return FileInputStream from file in plugin folder
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public InputStream getPluginFile(String folder, String resource) throws FileNotFoundException, IOException {
+		return new FileInputStream(new File("plugins/" + folder, resource));
+	}
+	
+	/**
+	 * 
+	 * @param folder
+	 * @param resource
+	 * @return URL to file in plugin folder
+	 * @throws MalformedURLException
+	 */
+	public URL getPluginUrl(String folder, String resource) throws MalformedURLException {
+		return new URL("plugins/" + folder + "/" + resource);
 	}
 }
