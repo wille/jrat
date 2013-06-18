10e68e662dd7f815f3839568ec1973a2f0d50b92
redpois0n <red@cmail.nu>
2013-06-18 18:48:57 +0200

    PacketBuilder improving

--- src/org/jrat/project/api/PacketBuilder.java
+++ src/org/jrat/project/api/PacketBuilder.java
@@ -3,10 +3,22 @@ package org.jrat.project.api;
 import java.io.DataOutputStream;
 
 public abstract class PacketBuilder {
+	
+	private DataOutputStream dos;
+	private byte header;
+	
+	public PacketBuilder(DataOutputStream dos, byte header) {
+		this.dos = dos;
+		this.header = header;
+	}
 		
 	public abstract void write(DataOutputStream dos) throws Exception;
 	
-	public final void writeString(DataOutputStream dos, String s) throws Exception {
+	public final void writeHeader() throws Exception {
+		dos.writeByte(header);
+	}
+	
+	public final void writeString(String s) throws Exception {
 		dos.writeShort(s.length());
 		dos.writeChars(s);
 	}
