5803f4d663dc81dbad8839cf7d41893faebac959
redpois0n <red@cmail.nu>
2013-06-28 12:20:38 +0200

    OnlinePlugin

--- Client/src/pro/jrat/extensions/OnlinePlugin.java
+++ Client/src/pro/jrat/extensions/OnlinePlugin.java
@@ -1,6 +1,14 @@
 package pro.jrat.extensions;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.ImageIcon;
+
+import pro.jrat.Constants;
+import pro.jrat.net.WebRequest;
 
 public class OnlinePlugin {
 
@@ -11,8 +19,34 @@ public class OnlinePlugin {
 	private String author;
 	private ImageIcon icon;
 	
-	public static void getAvailablePlugins() {
+	public OnlinePlugin(String name, String version, String builtFor, String description, String author) {
+		this.name = name;
+		this.version = version;
+		this.builtFor = builtFor;
+		this.description = description;
+		this.author = author;
+	}
+	
+	public ImageIcon getIcon() {
+		if (icon == null) {
+			
+		}
 		
+		return icon;
+	}
+
+	public static List<OnlinePlugin> getAvailablePlugins() throws Exception {
+		List<OnlinePlugin> plugins = new ArrayList<OnlinePlugin>();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(WebRequest.getInputStream(Constants.HOST + "/plugins/plugins.php")));
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			
+		}
+		
+		
+		reader.close();
 	}
 
 }
--- Client/src/pro/jrat/ui/frames/FrameOnlinePlugins.java
+++ Client/src/pro/jrat/ui/frames/FrameOnlinePlugins.java
@@ -1,25 +1,80 @@
 package pro.jrat.ui.frames;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Toolkit;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
 public class FrameOnlinePlugins extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private DefaultTableModel model;
+	private JPopupMenu popupMenu;
+	private JMenuItem mntmReload;
 
 	public FrameOnlinePlugins() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(FrameOnlinePlugins.class.getResource("/icons/application_icon_large.png")));
 		setTitle("Browse Online Plugins");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 725, 418);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+
+		table = new JTable();
+		model = new DefaultTableModel(new Object[][] {}, new String[] { "Name", "Author", "Description", "Version", "Built for" }) {
+			public boolean isCellEditable(int i, int i1) {
+				return false;
+			}
+		};
+		
+		table.setModel(model);
+		table.getColumnModel().getColumn(0).setPreferredWidth(144);
+		table.getColumnModel().getColumn(1).setPreferredWidth(93);
+		table.getColumnModel().getColumn(2).setPreferredWidth(124);
+		
+		popupMenu = new JPopupMenu();
+		addPopup(scrollPane, popupMenu);
+		addPopup(table, popupMenu);
+		
+		mntmReload = new JMenuItem("Reload");
+		popupMenu.add(mntmReload);
+		scrollPane.setViewportView(table);
 	}
 
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
