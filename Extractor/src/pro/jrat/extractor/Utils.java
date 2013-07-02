26a2b9e3aebd2eff66a37a8d50833b30b8575d5f
redpois0n <red@cmail.nu>
2013-07-02 16:05:38 +0200

    Extractor removed to own repo

--- Extractor/src/pro/jrat/extractor/Utils.java
+++
@@ -1,9 +0,0 @@
-package pro.jrat.extractor;
-
-public class Utils {
-
-	public static int getPercentFromTotal(int subtotal, int total) {
-		return (int) (((float) subtotal / (float) total) * 100);
-	}
-	
-}
