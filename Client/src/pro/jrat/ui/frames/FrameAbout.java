98c74cefcceff36718c79cdb750ac16a76037b85
redpois0n <red@cmail.nu>
2013-07-03 16:23:46 +0200

    FrameAbout lists contributors

--- Client/src/pro/jrat/ui/frames/FrameAbout.java
+++ Client/src/pro/jrat/ui/frames/FrameAbout.java
@@ -1,6 +1,7 @@
 package pro.jrat.ui.frames;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -10,22 +11,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ListModel;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 
 import pro.jrat.Constants;
+import pro.jrat.Contributors;
+import pro.jrat.Contributors.Contributor;
 import pro.jrat.Main;
 import pro.jrat.common.Version;
+import pro.jrat.utils.FlagUtils;
 import pro.jrat.utils.NetworkUtils;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 @SuppressWarnings("serial")
 public class FrameAbout extends JFrame {
@@ -42,6 +53,7 @@ public class FrameAbout extends JFrame {
 	}
 
 	private JPanel contentPane;
+	private DefaultListModel<String> model;
 
 	public FrameAbout() {
 		super();
@@ -109,49 +121,41 @@ public class FrameAbout extends JFrame {
 		});
 		lblRedpoisn.setIcon(new ImageIcon(FrameAbout.class.getResource("/icons/action_disconnect.png")));
 		lblRedpoisn.setForeground(Color.RED);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(183, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnClose, GroupLayout.PREFERRED_SIZE, 79, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-									.addComponent(lblWebsite, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
-									.addComponent(lblWrittenBy, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE))
-								.addComponent(lblPointOfOrigin)
-								.addComponent(lblJrat))
-							.addGap(18)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblSweden, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(lblHttpredpoisncom, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(lblRedpoisn, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)))))
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(28)
-					.addComponent(lblJrat)
-					.addGap(96)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblSweden, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblPointOfOrigin, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblWebsite, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblHttpredpoisncom))
-					.addGap(7)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblRedpoisn)
-						.addComponent(lblWrittenBy, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnClose, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
-		);
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(gl_contentPane.createSequentialGroup().addContainerGap(173, Short.MAX_VALUE).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 243, GroupLayout.PREFERRED_SIZE).addComponent(lblJrat).addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addComponent(btnClose, GroupLayout.PREFERRED_SIZE, 79, GroupLayout.PREFERRED_SIZE).addGroup(gl_contentPane.createSequentialGroup().addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addComponent(lblWebsite, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE).addComponent(lblWrittenBy, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)).addComponent(lblPointOfOrigin)).addGap(18).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblSweden, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE).addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false).addComponent(lblHttpredpoisncom, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addComponent(lblRedpoisn, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)))))).addContainerGap()));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addContainerGap().addComponent(lblJrat).addPreferredGap(ComponentPlacement.RELATED).addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE).addPreferredGap(ComponentPlacement.RELATED).addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblSweden, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE).addComponent(lblPointOfOrigin, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)).addPreferredGap(ComponentPlacement.UNRELATED).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblWebsite, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE).addComponent(lblHttpredpoisncom)).addGap(7).addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblRedpoisn).addComponent(lblWrittenBy, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)).addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnClose, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE).addContainerGap()));
+
+		JList<String> list = new JList<String>();
+		final List<Contributor> contributors = new Contributors().getContributors();
+
+		model = new DefaultListModel<String>();
+
+		list.setModel((ListModel<String>) model);
+		for (Contributor contributor : contributors) {
+			model.addElement(contributor.getName() + " - " + contributor.getReason());
+		}
+		list.setCellRenderer(new DefaultListCellRenderer() {
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+				super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+				Contributor contributor = null;
+
+				for (Contributor c : contributors) {
+					if (c.getName().equals(value.toString().split(" - ")[0])) {
+						contributor = c;
+						break;
+					}
+				}
+
+				setIcon(FlagUtils.getFlag(contributor.getCountry()));
+
+				return this;
+			}
+		});
+		scrollPane.setViewportView(list);
 		contentPane.setLayout(gl_contentPane);
 
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
