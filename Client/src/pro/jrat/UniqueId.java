b035971892b4f13d618f1cda58563a88f58c2264
redpois0n <red@cmail.nu>
2013-07-12 12:57:28 +0200

    UniqueId checking

--- Client/src/pro/jrat/UniqueId.java
+++ Client/src/pro/jrat/UniqueId.java
@@ -2,19 +2,27 @@ package pro.jrat;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
 
+import pro.jrat.common.codec.Hex;
 import pro.jrat.common.hash.Sha256;
+import pro.jrat.exceptions.InvalidKeyException;
+import pro.jrat.exceptions.MissingKeyException;
 
 public final class UniqueId {
 	
 	private static byte[] systemId = null;
 
 	public static final byte[] generateBinary() throws Exception {
-		String name = System.getProperty("user.name");
-		String os = System.getProperty("os.name") + " " + System.getProperty("os.version");
-		String country = System.getProperty("user.country") + " " + System.getProperty("user.country.format") + " " + System.getProperty("user.language");
-
-		String data = name + "+" + os + "+" + country;
+		Random rn = new Random();
+		
+		String data = "";
+		
+		for (int i = 0; i < 256; i++) {
+			data += rn.nextInt();
+		}
 
 		return Sha256.sha256(data.getBytes("UTF-8"));
 	}
@@ -22,6 +30,11 @@ public final class UniqueId {
 	public static byte[] getSystemId() throws Exception {
 		if (systemId == null) {
 			File file = new File("jrat.key");
+			
+			if (!file.exists()) {
+				throw new MissingKeyException("Key file not found (jrat.key)");
+			}
+			
 			FileInputStream input = new FileInputStream(file);
 			systemId = new byte[32];
 			input.read(systemId);
@@ -29,6 +42,25 @@ public final class UniqueId {
 		}
 		
 		return systemId;
+	}
+	
+	public static boolean validate() throws MissingKeyException, KeyNotFoundException, InvalidKeyException, Exception {
+		System.out.println("Checking key...");
+		
+		byte[] id = getSystemId();
+				
+		HttpURLConnection archiveConnection = (HttpURLConnection) new URL(Constants.HOST + "/checkid.php?id=" + Hex.encode(id)).openConnection();
+		archiveConnection.connect();
+		
+		int response = archiveConnection.getResponseCode();
+		
+		if (response == 403 || response == 404) {
+			throw new InvalidKeyException("Invalid key");
+		} else if (response == 200) {
+			return true;
+		}
+		
+		
 	}
 
 }
