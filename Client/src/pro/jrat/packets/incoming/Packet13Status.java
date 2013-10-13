042bcb4df482d1fea409d04b517a721465b13712
redpois0n <red@cmail.nu>
2013-10-13 11:06:47 +0200

    String as error message (status) not showing fixed

--- Client/src/pro/jrat/packets/incoming/Packet13Status.java
+++ Client/src/pro/jrat/packets/incoming/Packet13Status.java
@@ -13,9 +13,10 @@ public class Packet13Status extends AbstractIncomingPacket {
 	@Override
 	public void read(Slave slave, DataInputStream dis) throws Exception {
 		String status = slave.readLine();
-		int istatus = Integer.parseInt(status);
 		
 		try {
+			int istatus = Integer.parseInt(status);
+
 			Frame.mainModel.setValueAt(Status.getStatusFromID(istatus), Utils.getRow(3, slave.getIP()), 2);
 			slave.setStatus(istatus);
 		} catch (Exception ex) {
