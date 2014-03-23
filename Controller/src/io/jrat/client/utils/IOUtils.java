b7632deb8b578eed7e15e14bca29f3edc95da830
redpois0n <red@cmail.nu>
2014-03-23 10:59:54 +0100

    More IOUtils

--- Controller/src/io/jrat/client/utils/IOUtils.java
+++ Controller/src/io/jrat/client/utils/IOUtils.java
@@ -1,13 +1,15 @@
 package io.jrat.client.utils;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class IOUtils {
 
-	public static String readString(InputStream is) throws IOException {
+	public static String readString(InputStream is) throws Exception {
 		char[] buf = new char[2048];
 		Reader r = new InputStreamReader(is, "UTF-8");
 		StringBuilder s = new StringBuilder();
@@ -20,4 +22,19 @@ public class IOUtils {
 		return s.toString();
 	}
 
+	public static void writeFile(File file, byte[] bytes) throws Exception {
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(bytes);
+		fos.close();
+	}
+	
+	public static byte[] readFile(File file) throws Exception {
+		byte[] array = new byte[(int) file.length()];
+		
+		FileInputStream fis = new FileInputStream(file);	
+		fis.read(array);	
+		fis.close();
+		
+		return array;
+	}
 }
