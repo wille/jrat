dbff5fcb09df1dd3e2dff4215f5530b487483455
redpois0n <red@cmail.nu>
2014-05-17 14:56:14 +0200

    FramePerformance contains working dir, more to come

--- Controller/src/io/jrat/client/ui/frames/FramePerformance.java
+++ Controller/src/io/jrat/client/ui/frames/FramePerformance.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,6 +14,10 @@ import javax.swing.JLabel;
 
 import com.redpois0n.graphs.taskmgr.TaskmgrColors;
 import com.redpois0n.graphs.taskmgr.TaskmgrGraph;
+
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 @SuppressWarnings("serial")
 public class FramePerformance extends BaseFrame {
@@ -31,14 +36,14 @@ public class FramePerformance extends BaseFrame {
 
 		setTitle("Performance");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Frame.class.getResource("/icons/meter.png")));
-		setLayout(null);
+		getContentPane().setLayout(null);
 		setResizable(false);
-		setBounds(100, 100, 600, 330);
+		setBounds(100, 100, 600, 426);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		final JLabel lblMaximum = new JLabel("Maximum: ");
 		lblMaximum.setBounds(150, 270, 100, 10);
-		add(lblMaximum);
+		getContentPane().add(lblMaximum);
 
 		JButton button = new JButton("Garbage Collect");
 		button.setBounds(10, 260, 130, 30);
@@ -50,11 +55,34 @@ public class FramePerformance extends BaseFrame {
 			}
 		});
 
-		add(button);
+		getContentPane().add(button);
 
 		graph = new TaskmgrGraph(new TaskmgrColors());
 		graph.setBounds(0, 0, 593, 250);
-		add(graph);
+		getContentPane().add(graph);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createTitledBorder("Runtime info"));
+		panel.setBounds(10, 301, 574, 85);
+		getContentPane().add(panel);
+		
+		JLabel lblWorkingDirectory = new JLabel("Working Directory: " + System.getProperty("user.dir"));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblWorkingDirectory)
+					.addContainerGap(264, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblWorkingDirectory)
+					.addContainerGap(37, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
 
 		new Thread("Memory thread") {
 			@Override
@@ -81,5 +109,4 @@ public class FramePerformance extends BaseFrame {
 			}
 		}.start();
 	}
-
 }
