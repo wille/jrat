a3ac47a22bfa0acb0194b1fd84783505db73b450
redpois0n <red@cmail.nu>
2014-06-15 12:21:34 +0200

    Main in proper order

--- Updater/src/su/jrat/updater/Main.java
+++ Updater/src/su/jrat/updater/Main.java
@@ -1,9 +1,23 @@
 package su.jrat.updater;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		try {
+			if (!new File(Utils.getJarFile().getParentFile().getParentFile(), "Controller.jar").exists()) {
+				Logger.log("Not archive installation");
+				System.exit(0);
+			}
+			
+			Updater updater = new Updater();
+			updater.deleteFiles();
+			Downloader dl = new Downloader();
+			dl.update();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
