af22094dc49b5e4986702bea9fa8c58d45825312
redpois0n <red@cmail.nu>
2014-06-15 12:40:16 +0200

    Executes Controller.jar

--- Updater/src/su/jrat/updater/Main.java
+++ Updater/src/su/jrat/updater/Main.java
@@ -3,10 +3,21 @@ package su.jrat.updater;
 import java.io.File;
 
 public class Main {
+	
+	public static final String JAVA_HOME;
+
+	static {
+		if (System.getProperty("os.name").toLowerCase().contains("win")) {
+			JAVA_HOME = System.getProperty("java.home") + "\\bin\\javaw.exe";
+		} else {
+			JAVA_HOME = System.getProperty("java.home") + "/bin/java";
+		}
+	}
 
 	public static void main(String[] args) {
 		try {
-			if (!new File(Utils.getJarFile().getParentFile().getParentFile(), "Controller.jar").exists()) {
+			File controller = new File(Utils.getJarFile().getParentFile().getParentFile(), "Controller.jar");
+			if (!controller.exists()) {
 				Logger.log("Not archive installation");
 				System.exit(0);
 			}
@@ -15,9 +26,10 @@ public class Main {
 			updater.deleteFiles();
 			Downloader dl = new Downloader();
 			dl.update();			
+			
+			Runtime.getRuntime().exec(new String[] { JAVA_HOME, "-jar", controller.getAbsolutePath() });
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
