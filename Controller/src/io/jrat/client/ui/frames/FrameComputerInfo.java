5c3c309785fde9d4220a9b1f3aa476a9b12791f9
redpois0n <red@cmail.nu>
2014-12-05 18:56:21 +0100

    FrameComputerInfo exit on close fixed

--- Controller/src/io/jrat/client/ui/frames/FrameComputerInfo.java
+++ Controller/src/io/jrat/client/ui/frames/FrameComputerInfo.java
@@ -15,7 +15,7 @@ public class FrameComputerInfo extends JFrame {
 	private JPanel contentPane;
 
 	public FrameComputerInfo(Slave slave) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
