534819be1eae1b44b13796b993f081610b61da68
redpois0n <red@cmail.nu>
2014-12-07 16:36:33 +0100

    Packet range changed

--- Common/src/se/jrat/common/PacketRange.java
+++ Common/src/se/jrat/common/PacketRange.java
@@ -2,9 +2,9 @@ package se.jrat.common;
 
 public final class PacketRange {
 
-	public static final int outgoingRange = 100;
+	public static final int outgoingRange = 110;
 
-	public static final int incomingRange = 100;
+	public static final int incomingRange = 110;
 
 	public static final int incomingStubRange = outgoingRange;
 	public static final int outgoingStubRange = incomingRange;
