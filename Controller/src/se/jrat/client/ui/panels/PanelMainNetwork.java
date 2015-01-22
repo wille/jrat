346fe12a82ff2c30fdd4d83730e02a14c3f6a481
redpois0n <red@cmail.nu>
2015-01-22 18:25:13 +0100

    Network graph implemented correctly

--- Controller/src/se/jrat/client/ui/panels/PanelMainNetwork.java
+++ Controller/src/se/jrat/client/ui/panels/PanelMainNetwork.java
@@ -1,9 +1,9 @@
 package se.jrat.client.ui.panels;
 
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import se.jrat.client.listeners.NetworkMonitorListener;
 import se.jrat.client.threads.RunnableNetworkCounter;
@@ -15,10 +15,7 @@ import com.redpois0n.graphs.network.NetworkGraph;
 public class PanelMainNetwork extends JPanel {
 
 	private NetworkGraph graph;
-
-	private int width = 630;
-	private int height = 320;
-
+	
 	private boolean initialized;
 	
 	private Listener listener = new Listener();
@@ -28,51 +25,22 @@ public class PanelMainNetwork extends JPanel {
 	}
 
 	public void init() {
-		setLayout(null);
+		RunnableNetworkCounter.addListener(listener);
 
 		if (!initialized) {
 			initialized = true;
 
+			setBorder(new EmptyBorder(5, 5, 5, 5));
+			setLayout(new BorderLayout(0, 0));
+			
 			graph = new NetworkGraph(new NetworkColors());
 
-			add(graph);
+			add(graph, BorderLayout.CENTER);
 		}
-
-		graph.setBounds(5, 5, this.getWidth() - 5, this.getHeight() - 5);
-
-		addComponentListener(new ComponentListener() {
-
-			@Override
-			public void componentResized(ComponentEvent e) {
-				boolean b = width == getWidth() && height == getHeight();
-				width = getWidth();
-				height = getHeight();
-
-				if (!b) {
-					init();
-				}
-			}
-
-			@Override
-			public void componentHidden(ComponentEvent arg0) {
-
-			}
-
-			@Override
-			public void componentMoved(ComponentEvent arg0) {
-
-			}
-
-			@Override
-			public void componentShown(ComponentEvent arg0) {
-
-			}
-		});
 	}
 
 	public void setActive(boolean b) {
 		if (b) {
-			RunnableNetworkCounter.addListener(listener);
 		} else {
 			RunnableNetworkCounter.removeListener(listener);
 		}
