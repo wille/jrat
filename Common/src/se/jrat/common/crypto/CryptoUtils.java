29d48d8e5242aebb067d85b4dc096dff357fd315
redpois0n <red@cmail.nu>
2015-02-18 13:30:07 +0100

    Get stream and block cipher (Stream for Sockets, Block for everything else)

--- Common/src/se/jrat/common/crypto/CryptoUtils.java
+++ Common/src/se/jrat/common/crypto/CryptoUtils.java
@@ -8,10 +8,17 @@ import javax.crypto.spec.IvParameterSpec;
 public class CryptoUtils {
 	
 	public static final byte[] iv = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
-	public static final String STANDARD_ALGO = "AES/CTR/PKCS5PADDING";
+	public static final String STANDARD_STREAM_ALGO = "AES/CTR/PKCS5PADDING";
+	public static final String STANDARD_BLOCK_ALGO = "AES/CBC/PKCS5PADDING";
 	
-	public static Cipher getCipher(int mode, Key key) throws Exception {
-		Cipher cipher = getCipher(mode, STANDARD_ALGO, key, getRandomIv(16));
+	public static Cipher getStreamCipher(int mode, Key key) throws Exception {
+		Cipher cipher = getCipher(mode, STANDARD_STREAM_ALGO, key, getRandomIv(16));
+		
+		return cipher;
+	}
+	
+	public static Cipher getBlockCipher(int mode, Key key) throws Exception {
+		Cipher cipher = getCipher(mode, STANDARD_BLOCK_ALGO, key, getRandomIv(16));
 		
 		return cipher;
 	}
