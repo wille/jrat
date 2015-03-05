fe9c58006d1a6078357071b54f4cd19d907b68cd
redpois0n <red@cmail.nu>
2015-03-05 21:26:49 +0100

    String changed in debug message build panel

--- Controller/src/se/jrat/client/ui/panels/PanelBuildDebugMessages.java
+++ Controller/src/se/jrat/client/ui/panels/PanelBuildDebugMessages.java
@@ -31,7 +31,7 @@ public class PanelBuildDebugMessages extends JPanel {
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(PanelBuildDebugMessages.class.getResource("/icons/application-detail.png")));
 
-		JLabel lblWillPrintOut = new JLabel("Will print out debug messages that the developer left");
+		JLabel lblWillPrintOut = new JLabel("Will print out debug messages");
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel.createSequentialGroup().addGap(22).addComponent(label).addPreferredGap(ComponentPlacement.RELATED).addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblWillPrintOut).addComponent(chckbxKeepDebugMessages)).addContainerGap(118, Short.MAX_VALUE)));
 		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel.createSequentialGroup().addContainerGap().addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false).addComponent(label, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addComponent(chckbxKeepDebugMessages, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)).addGap(18).addComponent(lblWillPrintOut).addContainerGap(187, Short.MAX_VALUE)));
