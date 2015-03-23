0f9a99afbc5435cd3890726c52e7766eef89b451
redpois0n <red@cmail.nu>
2015-03-23 08:51:05 +0100

    On Connect panel fixed, buttons visible again

--- Controller/src/se/jrat/client/ui/panels/PanelMainOnConnect.java
+++ Controller/src/se/jrat/client/ui/panels/PanelMainOnConnect.java
@@ -7,9 +7,9 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
 import se.jrat.client.AbstractSlave;
@@ -19,16 +19,17 @@ import se.jrat.client.events.Events;
 import se.jrat.client.ui.components.DefaultJTable;
 import se.jrat.client.utils.IconUtils;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JToolBar;
+
 @SuppressWarnings("serial")
-public class PanelMainOnConnect extends JScrollPane {
+public class PanelMainOnConnect extends JPanel {
 	
 	private JTable table;
 	private DefaultTableModel model;
 
 	public PanelMainOnConnect() {		
-		setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
 		final JComboBox<Object> boxOnConnect = new JComboBox<Object>();
 		boxOnConnect.setModel(new DefaultComboBoxModel<Object>(Events.events.toArray()));
 
@@ -112,8 +113,18 @@ public class PanelMainOnConnect extends JScrollPane {
 		table.getColumnModel().getColumn(2).setPreferredWidth(129);
 		table.getColumnModel().getColumn(3).setPreferredWidth(140);
 		table.getColumnModel().getColumn(4).setPreferredWidth(175);
+		setLayout(new BorderLayout(0, 0));
 		table.setRowHeight(20);
-		setViewportView(table);
+		add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		add(toolBar, BorderLayout.NORTH);
+		
+		toolBar.add(btnAdd);
+		toolBar.add(btnDelete);
+		toolBar.add(btnEdit);
+		toolBar.add(btnPerform);
 	}
 
 	public void addRow(Object[] displayData) {
