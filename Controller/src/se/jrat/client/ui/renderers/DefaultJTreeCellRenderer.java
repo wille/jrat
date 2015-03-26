56774da8d7968dc3dc50580fb13fe5c04be7e32c
redpois0n <red@cmail.nu>
2015-03-26 20:34:21 +0100

    Fix tree renderer background color

--- Controller/src/se/jrat/client/ui/renderers/DefaultJTreeCellRenderer.java
+++ Controller/src/se/jrat/client/ui/renderers/DefaultJTreeCellRenderer.java
@@ -11,16 +11,16 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 public class DefaultJTreeCellRenderer extends DefaultTreeCellRenderer {
 
 	public static final Color SELECT_GRAY = new Color(191, 205, 219);
+	
+	public DefaultJTreeCellRenderer() {
+		super();
+		super.setBackgroundSelectionColor(SELECT_GRAY);
+		super.setBorderSelectionColor(Color.gray);
+	}
 
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
 		JLabel label = (JLabel) super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
 		
-		if (hasFocus) {
-			label.setBackground(SELECT_GRAY);
-		} else {
-			label.setBackground(Color.white);
-		}
-				
 		label.setForeground(Color.black);
 
 		return label;
