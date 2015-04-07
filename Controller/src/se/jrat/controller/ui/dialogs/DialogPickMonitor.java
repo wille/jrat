3b7e26a8c363d332c320507fccb40ae75eab98f1
redpois0n <red@cmail.nu>
2015-04-08 00:58:40 +0200

    Pick monitor dialog now has better layout and is resizable

--- Controller/src/se/jrat/controller/ui/dialogs/DialogPickMonitor.java
+++ Controller/src/se/jrat/controller/ui/dialogs/DialogPickMonitor.java
@@ -2,6 +2,7 @@ package se.jrat.controller.ui.dialogs;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -9,12 +10,10 @@ import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import se.jrat.controller.Slave;
 import se.jrat.controller.listeners.PickMonitorListener;
@@ -29,13 +28,13 @@ import com.redpois0n.graphs.monitors.RemoteMonitor;
 public class DialogPickMonitor extends JDialog {
 
 	public static final Map<Slave, DialogPickMonitor> instances = new HashMap<Slave, DialogPickMonitor>();
-	
-	private final JPanel contentPanel = new JPanel();
+
 	private PanelMonitors panelMonitors;
 	private JLabel label;
 	private JButton btnThumbnails;
 
 	public DialogPickMonitor(final Slave sl, final PickMonitorListener l) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(DialogPickMonitor.class.getResource("/icons/monitor-arrow.png")));
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent arg0) {
@@ -45,77 +44,72 @@ public class DialogPickMonitor extends JDialog {
 		instances.put(sl, this);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setTitle("Pick Monitor");
-		setResizable(false);
 		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
-		{
-			panelMonitors = new PanelMonitors(sl.getMonitors(), false);
+		getContentPane().setLayout(new BorderLayout(0, 0));
 
-			panelMonitors.addListener(new MonitorListener() {
-				@Override
-				public void onMonitorChange(RemoteMonitor monitor) {
-					PanelMonitor panel = null;
+		panelMonitors = new PanelMonitors(sl.getMonitors(), false);
 
-					for (PanelMonitor panel1 : panelMonitors.getPanels()) {
-						if (panel1.getMonitor().equals(monitor)) {
-							panel = panel1;
-							break;
-						}
-					}
+		panelMonitors.addListener(new MonitorListener() {
+			@Override
+			public void onMonitorChange(RemoteMonitor monitor) {
+				PanelMonitor panel = null;
 
-					if (panel != null) {
-						label.setText("X, Y: " + panel.getMonitor().getX() + ", " + panel.getMonitor().getY() + " - " + panel.getMonitor().getWidth() + "x" + panel.getMonitor().getHeight());
-						l.monitorPick(panel.getMonitor());
+				for (PanelMonitor panel1 : panelMonitors.getPanels()) {
+					if (panel1.getMonitor().equals(monitor)) {
+						panel = panel1;
+						break;
 					}
 				}
-			});
-			contentPanel.add(panelMonitors);
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						setVisible(false);
-						dispose();
-					}
-				});
-				{
-					label = new JLabel("");
-					buttonPane.add(label);
+
+				if (panel != null) {
+					label.setText("X, Y: " + panel.getMonitor().getX() + ", " + panel.getMonitor().getY() + " - " + panel.getMonitor().getWidth()
+							+ "x" + panel.getMonitor().getHeight());
+					l.monitorPick(panel.getMonitor());
 				}
-				{
-					btnThumbnails = new JButton("Thumbnails");
-					btnThumbnails.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent arg0) {
-							sl.addToSendQueue(new Packet75AllThumbnails());
-							btnThumbnails.setText("Reload");
-						}
-					});
-					buttonPane.add(btnThumbnails);
-				}
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						setVisible(false);
-						dispose();
-					}
-				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+		});
+
+		getContentPane().add(panelMonitors);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
 			}
-		}
+		});
+
+		label = new JLabel("");
+		buttonPane.add(label);
+
+		btnThumbnails = new JButton("Thumbnails");
+		btnThumbnails.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				sl.addToSendQueue(new Packet75AllThumbnails());
+				btnThumbnails.setText("Reload");
+			}
+		});
+		buttonPane.add(btnThumbnails);
+
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		cancelButton.setActionCommand("Cancel");
+		buttonPane.add(cancelButton);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
 	}
 
 	public PanelMonitors getPanelMonitors() {
