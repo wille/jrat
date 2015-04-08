6cba2d545b621d021075c140fc835469d10cabd8
redpois0n <red@cmail.nu>
2015-04-08 13:21:04 +0200

    FramePlugins fixed

--- Controller/src/se/jrat/controller/ui/frames/FramePlugins.java
+++ Controller/src/se/jrat/controller/ui/frames/FramePlugins.java
@@ -1,19 +1,16 @@
 package se.jrat.controller.ui.frames;
 
+import java.awt.BorderLayout;
 import java.awt.Desktop;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
 import javax.swing.table.DefaultTableModel;
 
 import jrat.api.events.OnDisableEvent;
@@ -27,30 +24,22 @@ import se.jrat.controller.utils.IconUtils;
 @SuppressWarnings("serial")
 public class FramePlugins extends JFrame {
 
-	private JPanel contentPane;
-	public JTable table;
-	public DefaultTableModel model;
+	private JTable table;
+	private DefaultTableModel model;
 
 	public FramePlugins() {
 		setTitle("Installed Plugins");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(FramePlugins.class.getResource("/icons/plugin.png")));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 585, 356);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 
 		JScrollPane scrollPane = new JScrollPane();
 
-		JButton btnReload = new JButton("Reload all");
-		btnReload.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				reload();
-			}
-		});
-		btnReload.setIcon(IconUtils.getIcon("plugin"));
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
 
 		JButton btnFolder = new JButton("Open Plugin Folder");
+		toolBar.add(btnFolder);
 		btnFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
@@ -61,22 +50,24 @@ public class FramePlugins extends JFrame {
 			}
 		});
 		btnFolder.setIcon(IconUtils.getIcon("folder-go"));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(gl_contentPane.createSequentialGroup().addComponent(btnFolder).addPreferredGap(ComponentPlacement.RELATED, 211, Short.MAX_VALUE).addComponent(btnReload)).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 379, Short.MAX_VALUE)).addGap(2)));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE).addPreferredGap(ComponentPlacement.RELATED).addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnReload).addComponent(btnFolder)).addGap(3)));
+
+		JButton btnReload = new JButton("Reload all");
+		toolBar.add(btnReload);
+		btnReload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				reload();
+			}
+		});
+		btnReload.setIcon(IconUtils.getIcon("plugin"));
 
 		table = new DefaultJTable();
 		table.setDefaultRenderer(Object.class, new PluginsTableRenderer());
-		table.setModel(model = new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Name", "Author", "Description", "Version", "Status"
-			}
-		));
+		table.setModel(model = new DefaultTableModel(new Object[][] {}, new String[] { "Name", "Author", "Description", "Version", "Status" }));
 		table.setRowHeight(25);
 		scrollPane.setViewportView(table);
-		contentPane.setLayout(gl_contentPane);
+		setLayout(new BorderLayout(0, 0));
+		add(toolBar, BorderLayout.SOUTH);
+		add(scrollPane);
 
 		addPlugins();
 	}
@@ -110,7 +101,7 @@ public class FramePlugins extends JFrame {
 	public void addPlugins() {
 		for (int i = 0; i < PluginLoader.plugins.size(); i++) {
 			Plugin p = PluginLoader.plugins.get(i);
-			model.addRow(new Object[] {  p.getName(), p.getVersion(), p.getAuthor(), p.getDescription(), Plugin.getStatusString(p.getStatus()) });
+			model.addRow(new Object[] { p.getName(), p.getAuthor(), p.getDescription(), p.getVersion(), Plugin.getStatusString(p.getStatus()) });
 		}
 	}
 }
