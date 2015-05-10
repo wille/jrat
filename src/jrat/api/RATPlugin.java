da2375f5c5e3f2e124dfb160d91f2e38e0e736d6
redpois0n <red@cmail.nu>
2015-05-10 14:33:21 +0200

    All methods removed except getGlobalActionListener()

--- src/jrat/api/RATPlugin.java
+++ src/jrat/api/RATPlugin.java
@@ -1,7 +1,6 @@
 package jrat.api;
 
 import java.awt.event.ActionListener;
-import java.util.List;
 
 public abstract class RATPlugin {
 
@@ -50,29 +49,9 @@ public abstract class RATPlugin {
 	public String getAuthor() {
 		return this.author;
 	}
-
-	/**
-	 * 
-	 * @return List of RATMenuItems that will be on the right click menu of the
-	 *         Main tab
-	 */
-
-	public abstract List<RATMenuItem> getMenuItems();
-
-	/**
-	 * 
-	 * @return List of RATControlMenuEntries that will be on the Control Panel
-	 *         tree list
-	 * @throws Exception
-	 */
-
-	public abstract List<RATControlMenuEntry> getControlTreeItems();
-
-	/**
-	 * Display about dialog maybe?
-	 * 
-	 * @return ActionListener for button, null if none
-	 */
-	public abstract ActionListener getGlobalMenuItemListener();
+	
+	public ActionListener getGlobalActionListener() {
+		return null;
+	}
 
 }
