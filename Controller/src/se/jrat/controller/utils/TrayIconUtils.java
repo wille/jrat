dd313bd3aaac934f2afd4ce268db0b5b5f9ca2fd
redpois0n <red@cmail.nu>
2015-06-03 15:47:35 +0200

    Unneeded import removed

--- Controller/src/se/jrat/controller/utils/TrayIconUtils.java
+++ Controller/src/se/jrat/controller/utils/TrayIconUtils.java
@@ -6,7 +6,6 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import se.jrat.controller.Main;
 import se.jrat.controller.settings.Settings;
