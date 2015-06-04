30ca2eb313b1a58bd54256f4fae3ed7cf35f6485
redpois0n <red@cmail.nu>
2015-06-04 22:41:49 +0200

    Globals comments

--- Controller/src/se/jrat/controller/Globals.java
+++ Controller/src/se/jrat/controller/Globals.java
@@ -4,58 +4,101 @@ import java.io.File;
 
 public class Globals {
 
+	/**
+	 * @return global file directory
+	 */
 	public static final File getFileDirectory() {
 		return new File("files/");
 	}
 
+	/**
+	 * @return help documents directory in {@link #getFileDirectory()}
+	 */
 	public static final File getHelpDocDirectory() {
 		return new File(getFileDirectory(), "help/");
 	}
 
+	/**
+	 * @return ZKM JARs directory in {@link #getFileDirectory()}
+	 */
 	public static final File getZKMDirectory() {
 		return new File(getFileDirectory(), "zkm/");
 	}
 
+	/**
+	 * @return plugin directory in {@link #getFileDirectory()}
+	 */
 	public static final File getPluginDirectory() {
 		return new File(getFileDirectory(), "plugins/");
 	}
 
+	/**
+	 * @return plugin stubs directory in {@link #getFileDirectory()}
+	 */
 	public static final File getPluginStubDirectory() {
 		return new File(getPluginDirectory(), "stubs/");
 	}
 	
+	/**
+	 * @return notes directory in {@link #getFileDirectory()}
+	 */
 	public static final File getNotesDirectory() {
 		return new File(getFileDirectory(), "notes/");
 	}
 	
+	/**
+	 * @return settings directory in {@link #getFileDirectory()}
+	 */
 	public static final File getSettingsDirectory() {
 		return new File(getFileDirectory(), "settings/");
 	}
 	
+	/**
+	 * @return keypair directory in {@link #getFileDirectory()}
+	 */
 	public static final File getRSAKeysDirectory() {
 		return new File(getFileDirectory(), "rsakeys/");
 	}
 	
+	/**
+	 * @return library directory in {@link #getFileDirectory()}
+	 */
 	public static final File getLibDirectory() {
 		return new File(getFileDirectory(), "lib/");
 	}
 	
+	/**
+	 * @return stub file in {@link #getFileDirectory()}
+	 */
 	public static final File getStub() {
 		return new File(getFileDirectory(), "Stub.jar");
 	}
 	
+	/**
+	 * @return default keyfile jrat.key in {@link #getFileDirectory()}
+	 */
 	public static final File getKeyFile() {
 		return getKeyFile("");
 	}
 	
-	public static final File getKeyFile(String nr) {
-		return new File(getFileDirectory(), "jrat" + nr + ".key");
+	/**
+	 * @param s string between jrat and .key
+	 * @return key file in {@link #getFileDirectory()}
+	 */
+	public static final File getKeyFile(String s) {
+		return new File(getFileDirectory(), "jrat" + s + ".key");
 	}
 	
+	/**
+	 * @return Updater file in {@link #getFileDirectory()}
+	 */
 	public static final File getUpdater() {
 		return new File(getFileDirectory(), "Updater.jar");
 	}
 	
+	/**
+	 * Create all directories
+	 */
 	public static void mkdirs() {
 		getFileDirectory().mkdirs();
 		getHelpDocDirectory().mkdirs();
