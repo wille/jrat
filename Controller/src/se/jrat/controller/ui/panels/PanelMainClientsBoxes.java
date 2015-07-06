cacc2a6025077cd552cefd43632bf85ac7072402
redpois0n <red@cmail.nu>
2015-07-07 00:57:59 +0200

    Begin work on box view

--- Controller/src/se/jrat/controller/ui/panels/PanelMainClientsBoxes.java
+++ Controller/src/se/jrat/controller/ui/panels/PanelMainClientsBoxes.java
@@ -3,6 +3,8 @@ package se.jrat.controller.ui.panels;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 
 import se.jrat.controller.AbstractSlave;
@@ -10,10 +12,12 @@ import se.jrat.controller.AbstractSlave;
 @SuppressWarnings("serial")
 public class PanelMainClientsBoxes extends PanelMainClients {
 	
-	public PanelMainClientsBoxes() {
+	private JDesktopPane pane;
 	
+	public PanelMainClientsBoxes() {
+		pane = new JDesktopPane();
 		
-		setViewportView(null);
+		setViewportView(pane);
 	}
 	
 	@Override
@@ -23,12 +27,20 @@ public class PanelMainClientsBoxes extends PanelMainClients {
 	
 	@Override
 	public void addSlave(AbstractSlave slave) {
-
+		pane.add(new SlaveBox(slave));
 	}
 	
 	@Override
 	public void removeSlave(AbstractSlave slave) {
-
+		for (JInternalFrame c : pane.getAllFrames()) {
+			assert c instanceof SlaveBox;
+			
+			SlaveBox box = (SlaveBox) c;
+			
+			if (box.getSlave().equals(slave)) {
+				pane.remove(c);
+			}
+		}
 	}
 
 	@Override
@@ -40,17 +52,38 @@ public class PanelMainClientsBoxes extends PanelMainClients {
 	public List<AbstractSlave> getSelectedSlaves() {
 		List<AbstractSlave> list = new ArrayList<AbstractSlave>();
 
+		AbstractSlave selected = getSelectedSlave();
+		
+		if (selected != null) {
+			list.add(selected);
+		}
+		
 		return list;
 	}
 	
 	@Override
 	public AbstractSlave getSelectedSlave() {
-		return null;
+		AbstractSlave selected = null;
+		
+		for (JInternalFrame c : pane.getAllFrames()) {
+			assert c instanceof SlaveBox;
+			
+			SlaveBox box = (SlaveBox) c;
+			
+			if (box.isSelected()) {
+				selected = box.getSlave();
+				break; // only one frame can be active
+			}
+		}
+		
+		return selected;
 	}
 	
 	@Override
 	public void clear() {
-		
+		for (JInternalFrame c : pane.getAllFrames()) {
+			pane.remove(c);
+		}
 	}
 
 	@Override
@@ -58,6 +91,19 @@ public class PanelMainClientsBoxes extends PanelMainClients {
 		JMenu menu = new JMenu("Customize");
 		
 		return menu;
+	}
+	
+	private class SlaveBox extends JInternalFrame {
+		
+		private AbstractSlave slave;
+		
+		public SlaveBox(AbstractSlave slave) {
+			this.slave = slave;
+		}
+		
+		public AbstractSlave getSlave() {
+			return this.slave;
+		}
 	}
 
 }
