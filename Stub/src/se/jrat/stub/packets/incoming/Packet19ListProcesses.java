36403ce0d1a25dc696a4425a19c0dd2c19b57e63
redpois0n <red@cmail.nu>
2015-07-11 13:43:17 +0200

    Runs powershell if vista or above (no parsing done)

--- Stub/src/se/jrat/stub/packets/incoming/Packet19ListProcesses.java
+++ Stub/src/se/jrat/stub/packets/incoming/Packet19ListProcesses.java
@@ -8,6 +8,8 @@ import se.jrat.stub.Connection;
 import se.jrat.stub.packets.outgoing.Packet25Process;
 
 import com.redpois0n.oslib.OperatingSystem;
+import com.redpois0n.oslib.windows.WindowsOperatingSystem;
+import com.redpois0n.oslib.windows.WindowsVersion;
 
 
 public class Packet19ListProcesses extends AbstractIncomingPacket {
@@ -17,13 +19,8 @@ public class Packet19ListProcesses extends AbstractIncomingPacket {
 		boolean icons = con.readBoolean();
 		
 		try {
-			Process p = null;
-			if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.WINDOWS) {
-				p = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
-			} else {
-				p = Runtime.getRuntime().exec("ps aux");
-			}
-
+			Process p = run();
+			
 			if (p != null) {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
@@ -43,21 +40,58 @@ public class Packet19ListProcesses extends AbstractIncomingPacket {
 		} catch (Exception err) {
 			err.printStackTrace();
 		}
-	} 
+	}
 	
+	/**
+	 * Executes and returns the process to read the data from
+	 * @return
+	 */
+	public static final Process run() throws Exception {
+		Process p;
+				
+		if (OperatingSystem.getOperatingSystem().isUnix()) {
+			p = Runtime.getRuntime().exec("ps aux");
+		} else {
+			assert OperatingSystem.getOperatingSystem() instanceof WindowsOperatingSystem;
+			
+			WindowsOperatingSystem wos = (WindowsOperatingSystem) OperatingSystem.getOperatingSystem();
+			WindowsVersion version = wos.getVersion();
+
+			if (version.ordinal() >= WindowsVersion.WINVISTA.ordinal()) {
+				p = Runtime.getRuntime().exec(new String[] { "powershell", "Get-Process * | Format-List -Property *" });
+			} else {
+				p = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
+			}
+		}
+		
+		return p;
+	}
+	
+	/**
+	 * Parses input data and gets only the wanted information
+	 * @param line
+	 * @return array to be sent back to controller
+	 */
 	public static final String[] parse(String line) {
 		String[] data = new String[4];
 		
 		if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.WINDOWS) {
-			line = line.replace("\"", "").replace("ÿ", "");
-			String[] args = line.split(",");
-
-			data[0] = args[0]; // name
-			data[1] = args[1]; // pid
-			data[2] = args[2]; // type / user
-			data[3] = args[4]; // memory usage
+			WindowsOperatingSystem wos = (WindowsOperatingSystem) OperatingSystem.getOperatingSystem();
+			WindowsVersion version = wos.getVersion();
 			
-			data[3] = DataUnits.getAsString(Long.parseLong(data[3].split(" ")[0]) * 1000);
+			if (version.ordinal() >= WindowsVersion.WINVISTA.ordinal()) {
+				
+			} else {
+				line = line.replace("\"", "").replace("ÿ", "");
+				String[] args = line.split(",");
+
+				data[0] = args[0]; // name
+				data[1] = args[1]; // pid
+				data[2] = args[2]; // type / user
+				data[3] = args[4]; // memory usage
+				
+				data[3] = DataUnits.getAsString(Long.parseLong(data[3].split(" ")[0]) * 1000);
+			}
 		} else {
 			line = line.trim().replaceAll("( )+", " ");
 			String[] args = line.split(" ");
