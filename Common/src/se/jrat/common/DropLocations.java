0b0f6acdbb372a99097d04a2fc2ed9758ea8e658
redpois0n <red@cmail.nu>
2015-07-23 15:38:48 +0200

    droplocation custom extension

--- Common/src/se/jrat/common/DropLocations.java
+++ Common/src/se/jrat/common/DropLocations.java
@@ -15,34 +15,37 @@ public class DropLocations {
 	public static final int DESKTOP = 3; // Desktop
 	public static final String[] STRINGS = new String[] { "root/C drive", "temp/documents (UNIX)", "appdata", "desktop" };
 
-	
 	public static File getFile(int i, String fileName) throws Exception {
+		return getFile(i, fileName, ".jar");
+	}
+	
+	public static File getFile(int i, String fileName, String extension) throws Exception {
 		File file = null;
 
 		AbstractOperatingSystem os = OperatingSystem.getOperatingSystem();
 
 		if (i == 0 || os.getType() == OperatingSystem.WINDOWS && ((WindowsOperatingSystem) os).getVersion() == WindowsVersion.WINXP) {
 			if (os.getType() == OperatingSystem.WINDOWS) {
-				file = new File("C:\\" + fileName + ".jar");
+				file = new File("C:\\" + fileName + extension);
 			} else {
-				file = new File("/" + fileName + ".jar");
+				file = new File("/" + fileName + extension);
 			}
 		} else if (i == 1) {
 			if (os.getType() == OperatingSystem.WINDOWS) {
-				file = File.createTempFile(fileName, ".jar");
+				file = File.createTempFile(fileName, extension);
 			} else {
-				file = new File(System.getProperty("user.home") + "/Documents/" + fileName + ".jar");
+				file = new File(System.getProperty("user.home") + "/Documents/" + fileName + extension);
 			}
 		} else if (i == 2) {			
 			if (os.getType() == OperatingSystem.WINDOWS) {
-				file = new File(System.getenv("APPDATA") + "\\" + fileName + ".jar");
+				file = new File(System.getenv("APPDATA") + "\\" + fileName + extension);
 			} else if (os.getType() == OperatingSystem.OSX) {
-				file = new File(System.getProperty("user.home") + "/Library/" + fileName + ".jar");
+				file = new File(System.getProperty("user.home") + "/Library/" + fileName + extension);
 			} else {
-				file = File.createTempFile(fileName, ".jar");
+				file = File.createTempFile(fileName, extension);
 			}
 		} else if (i == 3) {
-			file = new File(System.getProperty("user.home") + "/Desktop/" + fileName + ".jar");
+			file = new File(System.getProperty("user.home") + "/Desktop/" + fileName + extension);
 		}
 		
 		return file;
