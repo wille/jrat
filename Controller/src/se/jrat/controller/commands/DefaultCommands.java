b9aafaa0b2ece7ce88af242e533fe4fb2b76237a
redpois0n <red@cmail.nu>
2015-09-20 18:33:50 +0200

    Socket command added

--- Controller/src/se/jrat/controller/commands/DefaultCommands.java
+++ Controller/src/se/jrat/controller/commands/DefaultCommands.java
@@ -9,6 +9,7 @@ public class DefaultCommands {
 		Commands.COMMANDS.put("liststats", CommandListStatistics.class);
 		Commands.COMMANDS.put("addstats", CommandAddStatistics.class);
 		Commands.COMMANDS.put("help", CommandHelp.class);
+		Commands.COMMANDS.put("socket", CommandSocket.class);
 		Commands.COMMANDS.put("save", CommandSave.class);
 	}
 
