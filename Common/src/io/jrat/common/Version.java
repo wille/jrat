fd1e2c1e11dd2786494eb38c7f8ad8dfb0efee89
redpois0n <red@cmail.nu>
2016-03-13 12:35:50 +0100

    ---------- 5.2.1 ----------

--- Common/src/io/jrat/common/Version.java
+++ Common/src/io/jrat/common/Version.java
@@ -3,7 +3,7 @@ package io.jrat.common;
 public final class Version {
 
 	public static final String getVersion() {
-		return "5.2";
+		return "5.2.1";
 	}
 
 	public static final int getProtocolVersion() {
