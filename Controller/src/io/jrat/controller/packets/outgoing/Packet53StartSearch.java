02a5b8ba85155f97fbb2fb87e89d6bca03edb6c3
redpois0n <red@cmail.nu>
2016-07-22 21:42:44 +0200

    Change variable names

--- Controller/src/io/jrat/controller/packets/outgoing/Packet53StartSearch.java
+++ Controller/src/io/jrat/controller/packets/outgoing/Packet53StartSearch.java
@@ -1,25 +1,26 @@
 package io.jrat.controller.packets.outgoing;
 
 import io.jrat.controller.Slave;
+
 import java.io.DataOutputStream;
 
 
 public class Packet53StartSearch extends AbstractOutgoingPacket {
 
-	private String drive;
-	private String term;
+	private String searchRoot;
+	private String pattern;
 	private boolean pathContains;
 
-	public Packet53StartSearch(String drive, String term, boolean pathContains) {
-		this.drive = drive;
-		this.term = term;
+	public Packet53StartSearch(String searchRoot, String pattern, boolean pathContains) {
+		this.searchRoot = searchRoot;
+		this.pattern = pattern;
 		this.pathContains = pathContains;
 	}
 
 	@Override
 	public void write(Slave slave, DataOutputStream dos) throws Exception {
-		slave.writeLine(drive);
-		slave.writeLine(term);
+		slave.writeLine(searchRoot);
+		slave.writeLine(pattern);
 		dos.writeBoolean(pathContains);
 	}
 
--- Stub/src/io/jrat/stub/packets/incoming/Packet53StartSearch.java
+++ Stub/src/io/jrat/stub/packets/incoming/Packet53StartSearch.java
@@ -9,13 +9,15 @@ public class Packet53StartSearch extends AbstractIncomingPacket {
 
 	@Override
 	public void read(Connection con) throws Exception {
-		String start = con.readLine();
-		if (!start.endsWith(File.separator)) {
-			start = start + File.separator;
+		String searchRoot = con.readLine();
+
+		if (!searchRoot.endsWith(File.separator)) {
+			searchRoot += File.separator;
 		}
-		String what = con.readLine();
-		boolean dir = con.readBoolean();
-		new FileSearch(con, start, what, dir).start();
+
+		String pattern = con.readLine();
+		boolean searchPath = con.readBoolean();
+		new FileSearch(con, searchRoot, pattern, searchPath).start();
 	}
 
 }
