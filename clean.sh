80d7f2529947885801d30e5d00ac250879c470b6
redpois0n <red@cmail.nu>
2017-10-26 22:15:07 +0200

    remove scripts

--- clean.sh
+++
@@ -1,8 +0,0 @@
-FILES=(Controller/Controller.jar Controller/files/Stub.jar)
-ENTRIES=(Debug.class .project .gitignore .classpath *.md config.dat)
-
-for i in "${FILES[@]}"
-do
-   echo Processing $i
-   zip -d $i ${ENTRIES[@]}
-done
--- statistics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Total commits:"
-echo "   "$(git rev-list HEAD --count)
-echo "Commits by member:"
-git shortlog -s
-echo "Total lines:"
-echo "  "$(git ls-files | xargs cat | wc -l)
