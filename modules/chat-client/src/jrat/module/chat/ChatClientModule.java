597dfd512ae0de5f37e2f009820ab6e682bca588
redpois0n <red@cmail.nu>
2017-10-28 17:42:09 +0200

    initialize chat module

---
+++ modules/chat-client/src/jrat/module/chat/ChatClientModule.java
@@ -0,0 +1,10 @@
+package jrat.module.chat;
+
+import jrat.api.Module;
+
+public class ChatClientModule extends Module {
+
+    public void init() throws Exception {
+
+    }
+}
---
+++ modules/chat-controller/src/jrat/module/chat/ChatControllerModule.java
@@ -0,0 +1,10 @@
+package jrat.module.chat;
+
+import jrat.api.Module;
+
+public class ChatControllerModule extends Module {
+
+    public void init() throws Exception {
+        
+    }
+}
