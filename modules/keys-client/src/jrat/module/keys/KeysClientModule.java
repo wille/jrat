74951117f65f442b6947b75980bfb34f0467e6fa
redpois0n <red@cmail.nu>
2017-10-29 20:53:21 +0100

    init keys module

---
+++ modules/keys-client/src/jrat/module/keys/KeysClientModule.java
@@ -0,0 +1,10 @@
+package jrat.module.keys;
+
+import jrat.api.Module;
+
+public class KeysClientModule extends Module {
+
+    public void init() throws Exception {
+
+    }
+}
---
+++ modules/keys-controller/src/jrat/module/keys/KeysControllerModule.java
@@ -0,0 +1,10 @@
+package jrat.module.keys;
+
+import jrat.api.ControllerModule;
+
+public class KeysControllerModule extends ControllerModule {
+
+    public void init() throws Exception {
+
+    }
+}
