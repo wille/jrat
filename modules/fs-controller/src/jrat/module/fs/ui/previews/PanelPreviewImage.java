4c61f3e602f6f16263e007a1d969c47b4af9cc43
redpois0n <red@cmail.nu>
2017-11-14 20:21:49 +0100

    remove all components on preview image panel, click on the image to save

--- modules/fs-controller/src/jrat/module/fs/ui/previews/PanelPreviewImage.java
+++ modules/fs-controller/src/jrat/module/fs/ui/previews/PanelPreviewImage.java
@@ -11,8 +11,7 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,62 +23,43 @@ public class PanelPreviewImage extends PreviewPanel<BufferedImage> {
 	private String file;
 
 	private PanelImage imagePanel;
-	private JButton btnReloadImage;
 
 	public PanelPreviewImage(Slave slave, String f) {
 		super(slave, "Image Preview - " + f, Resources.getIcon("image"));
 		this.file = f;
 		this.slave = slave;
 
-		setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createLineBorder(Color.gray.brighter()));
-
-		btnReloadImage = new JButton("Reload Image");
-		btnReloadImage.setEnabled(false);
-		btnReloadImage.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				reload();
-				btnReloadImage.setEnabled(false);
-			}
-		});
-		btnReloadImage.setIcon(Resources.getIcon("update"));
-
-		JButton btnSave = new JButton("Save");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JFileChooser c = new JFileChooser();
-				c.showSaveDialog(null);
-				File file = c.getSelectedFile();
-				if (file != null) {
-					try {
-						ImageIO.write(imagePanel.getBufferedImage(), "png", file);
-					} catch (IOException e) {
-						JOptionPane.showMessageDialog(null, "Error saving image: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		btnSave.setIcon(Resources.getIcon("save"));
-		GroupLayout gl_contentPane = new GroupLayout(this);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addComponent(btnReloadImage).addPreferredGap(ComponentPlacement.RELATED).addComponent(btnSave).addGap(342)).addGroup(gl_contentPane.createSequentialGroup().addComponent(panel, GroupLayout.DEFAULT_SIZE, 532, Short.MAX_VALUE).addContainerGap()));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup().addComponent(panel, GroupLayout.DEFAULT_SIZE, 291, Short.MAX_VALUE).addPreferredGap(ComponentPlacement.UNRELATED).addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnReloadImage).addComponent(btnSave)).addGap(10)));
-		panel.setLayout(new BorderLayout(0, 0));
+		setLayout(new BorderLayout());
 
 		imagePanel = new PanelImage();
-		panel.add(imagePanel, BorderLayout.CENTER);
-		setLayout(gl_contentPane);
+		imagePanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                save();
+            }
+        });
+		add(imagePanel, BorderLayout.CENTER);
 
 		reload();
 	}
 
+    private void save() {
+        JFileChooser c = new JFileChooser();
+        c.showSaveDialog(null);
+        File file = c.getSelectedFile();
+        if (file != null) {
+            try {
+                ImageIO.write(imagePanel.getBufferedImage(), "png", file);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Error saving image: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            }
+        }
+    }
+
 	public void addData(BufferedImage image) {
         imagePanel.image = image;
         imagePanel.repaint();
-
-        btnReloadImage.setEnabled(true);
     }
 
 	public void reload() {
