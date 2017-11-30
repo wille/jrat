b3ee8e843cd24fbe40a162b8a4fc9ac02daea6d9
redpois0n <red@cmail.nu>
2017-11-30 22:07:32 +0100

    startup items view takes whole screen

--- modules/system-controller/src/jrat/module/system/ui/PanelStartup.java
+++ modules/system-controller/src/jrat/module/system/ui/PanelStartup.java
@@ -3,15 +3,12 @@ package jrat.module.system.ui;
 import jrat.api.Resources;
 import jrat.api.ui.ClientPanel;
 import jrat.controller.Slave;
-import jrat.module.system.packets.Packet78RegistryStartup;
 import jrat.controller.ui.DefaultJTable;
 import jrat.controller.ui.components.TableModel;
+import jrat.module.system.packets.Packet78RegistryStartup;
 
 import javax.swing.*;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 
 @SuppressWarnings("serial")
@@ -26,62 +23,18 @@ public class PanelStartup extends ClientPanel {
 	public PanelStartup(Slave s) {
 		super(s, "Registry Startup", Resources.getIcon("application-run"));
 
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-		JButton btnReloadList = new JButton("Reload list");
-		btnReloadList.setIcon(Resources.getIcon("update"));
-		btnReloadList.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				clear();
-				slave.addToSendQueue(new Packet78RegistryStartup());
-			}
-		});
-
-		JButton btnClear = new JButton("Clear");
-		btnClear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				clear();
-			}
-		});
-		btnClear.setIcon(Resources.getIcon("clear"));
-
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 599, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnReloadList)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnClear)
-					.addContainerGap(406, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 306, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnReloadList)
-						.addComponent(btnClear))
-					.addContainerGap(13, Short.MAX_VALUE))
-		);
+		setLayout(new BorderLayout());
 
         JTable table = new DefaultJTable();
 
 		table.setRowHeight(25);
 
 		table.setModel(model = new TableModel(new Object[][] {}, new String[] { "Application" }));
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setViewportView(table);
-		setLayout(groupLayout);
-	}
+        add(scrollPane, BorderLayout.CENTER);
 
-	private void clear() {
-		while (model.getRowCount() > 0) {
-			model.removeRow(0);
-		}
+		slave.addToSendQueue(new Packet78RegistryStartup());
 	}
 }
