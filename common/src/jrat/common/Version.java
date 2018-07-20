6ad4c8bef934550da4cfd5fc701fe9ddd9827d94
redpois0n <red@cmail.nu>
2018-07-20 20:17:42 +0200

    ---------- 6.0.0-rc.1 ----------

--- common/src/jrat/common/Version.java
+++ common/src/jrat/common/Version.java
@@ -3,7 +3,7 @@ package jrat.common;
 public final class Version {
 
 	public static final String getVersion() {
-		return "6.0.0-beta.1";
+		return "6.0.0-rc.1";
 	}
 
 	public static final int getProtocolVersion() {
